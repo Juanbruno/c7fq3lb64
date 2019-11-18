c6cc49e56387d5340e6f6525228e30250addfdb6
Juan bruno <jpruizk9@gmail.com>
2019-11-18 12:25:12 -0500

    controllers

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -6,6 +6,10 @@ class ExpensesController < ApplicationController
      if params[:concept].present?
 		@expenses = @expenses.where("concept LIKE ?", "%#{params[:concept]}%")
 	end
+	
+	if params[:category_id].present?
+    	@expenses = @expenses.where("category_id=?", params[:category_id])
+    end
 
   end
 end
