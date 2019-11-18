540bd6f5b0716ed16dce8a6721e44e7ed0635220
Juan bruno <jpruizk9@gmail.com>
2019-11-18 11:49:30 -0500

    omplementar filtro gastos

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,5 +1,10 @@
 class ExpensesController < ApplicationController
   def index
      @expenses = Expense.order("date DESC")
+
+     if params[:concept].present?
+		@expenses = @expenses.where("concept LIKE ?", "%#{params[:concept]}%")
+	end
+
   end
 end
