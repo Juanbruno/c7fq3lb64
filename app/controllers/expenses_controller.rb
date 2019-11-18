0243d4cff3d5a7bee53b5140b9b50e88867b3679
Juan bruno <jpruizk9@gmail.com>
2019-11-18 12:00:09 -0500

    complementar filtro gastos2

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,6 +1,7 @@
 class ExpensesController < ApplicationController
   def index
-     @expenses = Expense.order("date DESC")
+
+     @expenses = Expense.order("date DESC").where("user_id",current_user.id)
 
      if params[:concept].present?
 		@expenses = @expenses.where("concept LIKE ?", "%#{params[:concept]}%")
