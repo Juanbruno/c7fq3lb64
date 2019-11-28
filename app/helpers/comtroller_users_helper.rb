099d2ae7cd1151d01aa3617169891d488c634422
Juan bruno <jpruizk9@gmail.com>
2019-11-28 16:08:50 -0500

    crea user

---
+++ app/helpers/comtroller_users_helper.rb
@@ -0,0 +1,2 @@
+module ComtrollerUsersHelper
+end
---
+++ test/controllers/comtroller_users_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ComtrollerUsersControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
